acb3605b9eaa769ba8093d7e97de04008b0c18fa
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2021-05-17 21:55:52 +0200

    feat: guardians greater than 3999 GREEN

--- src/romanNumbers.js
+++ src/romanNumbers.js
@@ -1,7 +1,8 @@
-const isNotAValidNumber = (number) => number < 0;
+const isNegativeNumber = (number) => number < 0;
+const isGreaterThan3999 = (number) => number > 3999;
 
 function generate(number) {
-  if (isNotAValidNumber(number)) return 'ERROR';
+  if (isNegativeNumber(number) || isGreaterThan3999(number)) return 'ERROR';
 }
 
 module.exports = generate;
