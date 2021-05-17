a97ba3b08cc3390a42df1208ee2ac648a22bd815
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2021-05-17 21:54:07 +0200

    feat: guardians greater than 3999 RED

--- test/romanNumbers.test.js
+++ test/romanNumbers.test.js
@@ -4,4 +4,7 @@ describe('Function Interface', () => {
   test('should reject negative numbers', () => {
     expect(generate(-1)).toBe('ERROR');
   });
+  test('should reject numbers greater than 3999', () => {
+    expect(generate(4000)).toBe('ERROR');
+  });
 });
